239d670bd0ec7491856e036e6e112d35784ab8be
Teo <teo.assuncao@he-arc.ch>
2023-03-27 11:19:20 +0200

    premier commit

---
+++ src/main/java/org/app/App.java
@@ -0,0 +1,18 @@
+package org.app;
+
+import webservice.PublicationWebService;
+
+import javax.xml.ws.Endpoint;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+        Endpoint.publish("http://localhost:9000/HelloWorld", new PublicationWebService());
+    }
+}
